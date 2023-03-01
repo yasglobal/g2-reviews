2b3a7577a0e9b96cb0a020a40f5ecd8c69d9aa18
Raheel Alam <raheel.alam@yasglobal.com>
2023-03-01 16:42:29 +0500

    Updates Uninstall File

--- uninstall.php
+++ uninstall.php
@@ -1,13 +1,22 @@
 <?php
-if ( ! defined( 'ABSPATH' )) {
+/**
+ * G2 Reviews Uninstall.
+ *
+ * Delete settings and custom table on uninstalling the Plugin.
+ *
+ * @package G2Reviews
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     exit;
 }
-
-global $wpdb;
-$table_name = G2_REVIEWS_TABLE;
 
 // Delete the plugin options
 delete_option( 'g2_reviews_settings' );
 
 // Delete the review table
+global $wpdb;
+
+$table_name = $wpdb->prefix . 'g2_reviews';
 $wpdb->query( "DROP TABLE IF EXISTS $table_name" );
